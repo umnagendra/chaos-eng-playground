3b754d80f76c13950ee3d074b2c99ecaf778628e
Nagendra Mahesh <namahesh@cisco.com>
2021-02-01 17:41:33 +0530

    Basic Spring Boot app

---
+++ chaos-demo/src/test/java/xyz/nagendra/chaosdemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.nagendra.chaosdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
